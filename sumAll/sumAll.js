aa6fe7ecf8dff190dbfb551d531dc0e5ecc2dd2c
Sanders <esanders@cicoa_xchg>
2019-02-06 16:10:16 -0500

    adds only when a>b

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+var sum = 0
+if(a<b){
+  for(a;a=b;a++){
+    sum = sum + a;
+  }
+  return sum;
+}
+else if(a>b){
+      for(a;a=b;a--){
+        sum = sum + a;
+      }
+    return sum;
+  }
 }
 
 module.exports = sumAll
