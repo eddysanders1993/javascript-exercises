8d7444c9cac3c28dad3ad7cc96f441afd2455020
Edward Sanders <eddysanders1993@gmail.com>
2019-02-16 12:39:56 -0500

    functions properly

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function(word) {
 
+  let check = word.replace(/[^a-z]/gi,'');
+  let checkLowerCase=check.toLowerCase();
+  let wordarr=checkLowerCase.split('');
+  let wordReverseArr=wordarr.reverse();
+  let wordReverse=wordReverseArr.join('');
+
+  if(wordReverse==checkLowerCase){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = palindromes
