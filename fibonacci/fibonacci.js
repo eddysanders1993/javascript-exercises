846d69a71fee42efd3f098c6ba8c08e64e8606c3
Edward Sanders <eddysanders1993@gmail.com>
2019-02-17 18:24:21 -0500

    functions properly

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(f) {
+  let arr = [0,1];
+  let num = parseInt(f);
+  if(f < 0) {
+    return 'OOPS';
+  }
 
+  for(var i =0;i<f;i++){
+    arr.push(arr[arr.length-2]+arr[arr.length-1]);
+  }
+  return arr[f].valueOf();
 }
 
 module.exports = fibonacci
