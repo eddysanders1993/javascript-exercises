51581e532bb13b1cadb0980db11e6869a801fc95
Edward Sanders <eddysanders1993@gmail.com>
2019-02-21 15:06:54 -0500

    works fine
    you can pass an array as a parameter if you name it array

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,39 @@
-function add () {
-	
+function add (x,y) {
+return x + y;
 }
 
-function subtract () {
-	
+function subtract (x,y) {
+return x - y;
 }
 
-function sum () {
-	
+
+
+function sum (array) {
+	return array.reduce((accumulator,nextItem) => accumulator + nextItem,0);
+
+	}
+
+
+function multiply (array) {
+	return array.length
+	? array.reduce((accumulator,nextItem) => accumulator * nextItem)
+	: 0;
+	}
+
+
+function power(a,b) {
+return a**b;
 }
 
-function multiply () {
-	
+function factorial(a) {
+if(a==0){
+	return 1;
 }
-
-function power() {
-	
+let product = 1;
+for(var i = 1;i<=a;i++){
+	product = product * i;
 }
-
-function factorial() {
-	
+return product;
 }
 
 module.exports = {
